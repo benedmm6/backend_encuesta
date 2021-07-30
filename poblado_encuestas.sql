dde9668178600760e4069be6ba608c5a6964adb0
benedmm6 <edgar.mendez.fotografia97@gmail.com>
2021-07-30 12:27:07 -0500

    Add files via upload

---
+++ poblado_encuestas.sql
@@ -0,0 +1,27 @@
+use encuesta;
+
+/*========== CATEGORIAS ==========*/
+
+INSERT INTO categorias(id,nombre_categoria) VALUES(1,'Estatal');
+INSERT INTO categorias(id,nombre_categoria) VALUES(2,'Municipal');
+
+/*========== MUNICIPIOS ==========*/
+
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (1,'Balancán','iconos/balancan.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (2,'Cárdenas','iconos/cardenas_0.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (3,'Centla','iconos/centla.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (4,'centro','iconos/centro.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (5,'Comalcalco','iconos/comalcalco.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (6,'Cunduacán','iconos/cunduacan.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (7,'Emiliano Zapata','iconos/emiliano zapata.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (8,'Huimanguillo','iconos/huimanguillo.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (9,'Jalapa','iconos/jalapa.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (10,'Jalpa de Méndez','iconos/jalpa de mendez.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (11,'Jonuta','iconos/jonuta.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (12,'Macuspana','iconos/macuspana.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (13,'Nacajuca','iconos/nacajuca.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (14,'Paraíso','iconos/paraiso.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (15,'Tacotalpa','iconos/tacotalpa.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (16,'Teapa','iconos/teapa.png');
+INSERT INTO municipios (id,nombre_municipio,icono) VALUES (17,'Tenosique','iconos/tenosique.png');
+
